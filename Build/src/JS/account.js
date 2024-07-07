aa483a2dc0997c79448f2b74a1bb8f4b7b6db0c6
Muhammad Nouman <140593593+NoumanBunty@users.noreply.github.com>
2024-07-07 22:35:00 +0500

    Delete Build directory

--- Build/src/JS/account.js
+++
@@ -1,4 +0,0 @@
-let name = localStorage.getItem("set")
-document.getElementById("accountname").innerText = name
-document.getElementById("username").innerText = name
-console.log('name :>> ', name);
